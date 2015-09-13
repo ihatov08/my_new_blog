c8e21b0e4e84f225edf8de34a3c4a2a0eb0b6c11
吉田智哉 <yoshidatomoya@tom.local>
2015-09-13 10:09:58 +0900

    crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -10,14 +12,33 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to post_url(@post)
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+
   end
 
-  def delete
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redeirect_to posts_url
   end
+
+ private
+ def post_params
+  params.require(:post).permit(
+    :title,
+    :category,
+    :body
+    )
+end
+
 end
